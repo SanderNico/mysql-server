5af67189ab0708ce5f16c189692abe134c0e860f
Sander Nicolausson <sander-nico@hotmail.com>
2022-01-25 09:09:52 +0100

    Testing hardcoded selectivity for int. Take 6.

--- sql/join_optimizer/estimate_selectivity.cc
+++ sql/join_optimizer/estimate_selectivity.cc
@@ -126,7 +126,7 @@ double EstimateSelectivity(THD *thd, Item *condition, string *trace) {
       for (Field *field : {down_cast<Item_field *>(left)->field,
                            down_cast<Item_field *>(right)->field}) {
                                                           
-          if(field->type() == enum_field_types::MYSQL_TYPE_INT24){
+          if(field->type() == enum_field_types::MYSQL_TYPE_VARCHAR){
               yes = true;
           }
         selectivity =
