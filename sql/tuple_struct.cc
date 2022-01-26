bfc6296579ea03368282053578d0c334666ff3f1
jorgaas <jorgaas@stud.ntnu.no>
2022-01-26 12:56:28 +0100

    Tuple struct with vector

--- sql/tuple_struct.cc
+++ sql/tuple_struct.cc
@@ -15,7 +15,7 @@ InMemoryTuple::InMemoryTuple(vector <tuple<string, string, string, double> > Pub
 double InMemoryTuple::GetSelectivityForCondition(Item *condition){
     double selectivity = -1.0;
 
-    for(int it = 0; it < 7; it++){
+    for(std::vector< tuple<string, string, string, double>>::size_type it = 0; it != Content.size(); it++){
       std::size_t a = ItemToString(condition).find(get<0>(Content[it]));
       std::size_t b = ItemToString(condition).find(get<2>(Content[it]));
       if(a != std::string::npos && b != std::string::npos){
