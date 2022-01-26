c0fa68f52013ca497a61af41f6a102757a788a64
jorgaas <jorgaas@stud.ntnu.no>
2022-01-26 12:02:38 +0100

    Tuple struct take X+3

--- sql/tuple_struct.cc
+++ sql/tuple_struct.cc
@@ -13,10 +13,10 @@ InMemoryTuple::InMemoryTuple(tuple<string, string, string, double> PublicContent
 
 double InMemoryTuple::GetSelectivityForCondition(Item *condition){
     double selectivity = -1.0;
-    std::size_t a = ItemToString(condition).find(Content[0]);
-    std::size_t b = ItemToString(condition).find(Content[2]);
+    std::size_t a = ItemToString(condition).find(get<0>(Content));
+    std::size_t b = ItemToString(condition).find(get<2>(Content));
     if(a != std::string::npos && b != std::string::npos){
-      selectivity = Content[3];
+      selectivity = get<3>(Content);
     }
     return selectivity;
 }
