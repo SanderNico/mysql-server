af61a6b4fba4b5bc82c007f15f04255af86ad7d2
jorgaas <jorgaas@stud.ntnu.no>
2022-01-27 10:56:58 +0100

    Reading selectivities from file Take 1

--- sql/init.cc
+++ sql/init.cc
@@ -61,5 +61,5 @@ void unireg_init(ulong options) {
   specialflag = options; /* Set options from argv */
 
   auto Content = GetSelectivitiesFromFile("../../selectivities.csv");
-  InMemoryTuple::SetContent(Content);
+  std::InMemoryTuple::SetContent(Content);
 }
