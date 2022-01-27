3b06986b42d6bdf486909b0c303379b9d45f0e80
jorgaas <jorgaas@stud.ntnu.no>
2022-01-27 12:59:32 +0100

    Selectivities from file take X

--- sql/init.cc
+++ sql/init.cc
@@ -67,5 +67,5 @@ void unireg_init(ulong options) {
   specialflag = options; /* Set options from argv */
 
   auto Content = GetSelectivitiesFromFile("../../selectivities.csv");
-  InMemoryTuple::SetContent(Content);
+  SetContent(Content);
 }
