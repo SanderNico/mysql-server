81fd6e86bbd64937b4d119344c9c864879d9782f
Sander Nicolausson <sander-nico@hotmail.com>
2022-03-02 11:47:19 +0100

    Trying to implement CountMinSketch

--- sql/count_min_sketch/CountMinSketch.h
+++ sql/count_min_sketch/CountMinSketch.h
@@ -62,5 +62,5 @@ public:
   ~CountMinSketch();
 };
 
-extern std::map<std::string, std::string> Dictionary;
+extern std::map<std::pair<std::string, std::string>, CountMinSketch> Dictionary;
 #endif /* COUNTMINSKETCH */
