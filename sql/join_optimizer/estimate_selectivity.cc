db2a90151b7e0ad38c8928d0adc36bdf54bc194d
jorgaas <jorgaas@stud.ntnu.no>
2022-03-03 10:08:04 +0100

    implementing countminsketch in estimateselectivity

--- sql/join_optimizer/estimate_selectivity.cc
+++ sql/join_optimizer/estimate_selectivity.cc
@@ -128,7 +128,7 @@ double EstimateSelectivity(THD *thd, Item *condition, string *trace) {
         double selectivity = -1.0;
         for (Field *field : {down_cast<Item_field *>(left)->field,
                             down_cast<Item_field *>(right)->field}) {
-          std::string tableName = field->table_name;
+          std::string tableName = &field->table_name;
           std::string fieldName = field->field_name;
 
           printf("tableNAME: %s, fieldNAME: %s" , tableName, fieldName);
