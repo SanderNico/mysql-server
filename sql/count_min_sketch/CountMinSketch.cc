82ee54e49bb0f33fd7896e6bbab3c0f74a4b8fe9
Sander Nicolausson <sander-nico@hotmail.com>
2022-03-15 09:40:26 +0100

    Trying to figure out why point queries deviate

--- sql/count_min_sketch/CountMinSketch.cc
+++ sql/count_min_sketch/CountMinSketch.cc
@@ -73,6 +73,7 @@ void CountMinSketch::update(int item, int c) {
   unsigned int hashval = 0;
   for (unsigned int j = 0; j < depth; j++) {
     hashval = (hashes[j][0]*item+hashes[j][1])%LONG_PRIME%width;
+    printf("ROUND: %d, HASHVAL: %d\n", j, hashval);
     C[j][hashval] = C[j][hashval] + c;
   }
 }
